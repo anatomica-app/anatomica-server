8c9381e5aa70b44560a8db3920bffaacaebf0c67
ahmetozrahat <ahmetozrahat25@gmail.com>
2021-09-18 18:33:47 +0300

    Added using date strings for MySQL.

--- routes/feedbacks.js
+++ routes/feedbacks.js
@@ -9,7 +9,8 @@ const pool = mysql.createPool({
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE,
-    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
+    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+    dateStrings: true
 });
 
 // Fetching all the feedbacks.
--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,8 @@ const pool = mysql.createPool({
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE,
-    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
+    socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+    dateStrings: true
 });
 
 // Fetch user from id.
