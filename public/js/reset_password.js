0f8792e1a8078ec074e54733c292022adbd77bf1
ahmetozrahat <ahmetozrahat25@gmail.com>
2021-12-19 18:53:58 +0300

    Switched to new project.

--- public/js/reset_password.js
+++ public/js/reset_password.js
@@ -1,3 +1,6 @@
+const apiPrefix = 'https://quiz.api.anatomica-app.com/';
+const apiVersion = 'v1';
+
 $(document).ready(function () {
 
     let pagePath = window.location.pathname;
@@ -6,7 +9,7 @@ $(document).ready(function () {
     let id = paths[5];
     let token = paths[6];
 
-    $("#reset-password-form").submit(function (event) {
+    $('#reset-password-form').submit(function (event) {
         event.preventDefault();
 
         const password1 = $('#password').val();
@@ -16,98 +19,98 @@ $(document).ready(function () {
 
         if (password1 !== password2) {
             // Password mismatch.
-            $.toast({ 
+            $.toast({
                 heading: 'Uyarı',
-                text : 'Parolalar birbiriyle uyuşmuyor. Lütfen kontrol ediniz.', 
-                showHideTransition : 'fade',
-                bgColor : '#EDB95E',
-                textColor : '#fff',
-                allowToastClose : true,
-                hideAfter : 5000,
-                stack : 5,
-                textAlign : 'left',
-                position : 'top-right',
+                text: 'Parolalar birbiriyle uyuşmuyor. Lütfen kontrol ediniz.',
+                showHideTransition: 'fade',
+                bgColor: '#EDB95E',
+                textColor: '#fff',
+                allowToastClose: true,
+                hideAfter: 5000,
+                stack: 5,
+                textAlign: 'left',
+                position: 'top-right',
                 icon: 'error',
                 loader: false
-            });   
-        }else {
+            });
+        } else {
             // Passwords match.
             // Let's check if password is longer than 6 characters.
 
-            if (password1.length > 6){
+            if (password1.length > 6) {
                 // Password is okay.
                 let loading = loadingOverlay().activate();
-                let apiPrefix = "https://anatomica-scx43dzaka-ew.a.run.app/v1/";
-    
+                let apiURL = apiPrefix + apiVersion;
+
                 const data = {
                     id: id,
                     token: token,
                     password: password1
                 }
-    
+
                 $.ajax({
-                    url: apiPrefix + "users/password/update/",
-                    method: "PUT",
+                    url: apiURL + '/users/password/update/',
+                    method: 'PUT',
                     headers: {
-                        Accept: "application/json, text/plain, */*",
-                        "Content-Type": "application/json",
-                        Authorization: "Bearer " + token,
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + token,
                     },
                     data: JSON.stringify(data),
                     success: function (json) {
                         loadingOverlay().cancel(loading);
                         if (json.error !== true) {
-                            window.location.href = "https://anatomica-scx43dzaka-ew.a.run.app/page_templates/password_changed.html";
-                        }else {
-                            $.toast({ 
+                            window.location.href = apiPrefix + 'page_templates/password_changed.html';
+                        } else {
+                            $.toast({
                                 heading: 'Hata',
-                                text : json.message, 
-                                showHideTransition : 'fade',
-                                bgColor : '#FF0033',
-                                textColor : '#fff',
-                                allowToastClose : true,
-                                hideAfter : 5000,
-                                stack : 5,
-                                textAlign : 'left',
-                                position : 'top-right',
+                                text: json.message,
+                                showHideTransition: 'fade',
+                                bgColor: '#FF0033',
+                                textColor: '#fff',
+                                allowToastClose: true,
+                                hideAfter: 5000,
+                                stack: 5,
+                                textAlign: 'left',
+                                position: 'top-right',
                                 icon: 'error',
                                 loader: false
                             });
                         }
                     },
-                    error: function(request,status,errorThrown) {
+                    error: function (request, status, errorThrown) {
                         loadingOverlay().cancel(loading);
-                        $.toast({ 
+                        $.toast({
                             heading: 'Hata',
-                            text : 'Beklenmedik bir hata oluştu. Lütfen daha sonra tekrar deneyiniz.', 
-                            showHideTransition : 'fade',
-                            bgColor : '#FF0033',
-                            textColor : '#fff',
-                            allowToastClose : true,
-                            hideAfter : 5000,
-                            stack : 5,
-                            textAlign : 'left',
-                            position : 'top-right',
+                            text: 'Beklenmedik bir hata oluştu. Lütfen daha sonra tekrar deneyiniz.',
+                            showHideTransition: 'fade',
+                            bgColor: '#FF0033',
+                            textColor: '#fff',
+                            allowToastClose: true,
+                            hideAfter: 5000,
+                            stack: 5,
+                            textAlign: 'left',
+                            position: 'top-right',
                             icon: 'error',
                             loader: false
                         });
                     }
                 });
-            }else {
-                $.toast({ 
+            } else {
+                $.toast({
                     heading: 'Uyarı',
-                    text : 'Parolanız 6 karakterden daha uzun olmalıdır.', 
-                    showHideTransition : 'fade',
-                    bgColor : '#EDB95E',
-                    textColor : '#fff',
-                    allowToastClose : true,
-                    hideAfter : 5000,
-                    stack : 5,
-                    textAlign : 'left',
-                    position : 'top-right',
+                    text: 'Parolanız 6 karakterden daha uzun olmalıdır.',
+                    showHideTransition: 'fade',
+                    bgColor: '#EDB95E',
+                    textColor: '#fff',
+                    allowToastClose: true,
+                    hideAfter: 5000,
+                    stack: 5,
+                    textAlign: 'left',
+                    position: 'top-right',
                     icon: 'error',
                     loader: false
-                });   
+                });
             }
         }
     });
--- routes/constants.js
+++ routes/constants.js
@@ -7,5 +7,6 @@ This file holds the constant values for re-use.
 module.exports = Object.freeze({
     ACCOUNT_DEFAULT: 0,
     ACCOUNT_GOOGLE: 1,
-    ACCOUNT_APPLE: 2
+    ACCOUNT_APPLE: 2,
+    DEFAULT_STORAGE_BUCKET: 'anatomica-storage'
 });
