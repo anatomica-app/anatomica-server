be4efd6a9f0bc2b090fcb0de51321b5628e95631
Oktay⚡ <oktay.parlak.12@hotmail.com>
2022-10-15 20:29:11 +0300

    Added nodemon and prettier. Reformated js files.

--- database.js
+++ database.js
@@ -2,11 +2,11 @@ let mysql = require('mysql');
 
 // ***** MySQL Connection *****
 const pool = mysql.createPool({
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE,
-    host: process.env.INSTANCE_CONNECTION_NAME,
-    dateStrings: true
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE,
+  host: process.env.INSTANCE_CONNECTION_NAME,
+  dateStrings: true,
 });
 
 module.exports = pool;
--- routes/reports.js
+++ routes/reports.js
@@ -9,41 +9,48 @@ const pool = require('../database');
 
 // Create a new report.
 router.post('/', checkAuth, (req, res) => {
-    const schema = Joi.object({
-        userId: Joi.number().integer().required(),
-        classicId: Joi.number().integer().allow(null).required(),
-        picturedId: Joi.number().integer().allow(null).required(),
-        message: Joi.string().required()
-    });
+  const schema = Joi.object({
+    userId: Joi.number().integer().required(),
+    classicId: Joi.number().integer().allow(null).required(),
+    picturedId: Joi.number().integer().allow(null).required(),
+    message: Joi.string().required(),
+  });
 
-    const result = schema.validate(req.body);
-    if (result.error) return res.status(400).json({ message: result.error.details[0].message });
+  const result = schema.validate(req.body);
+  if (result.error)
+    return res.status(400).json({ message: result.error.details[0].message });
 
-    const sql = "CALL create_question_report(?, ?, ?, ?)";
-    data = [
-        req.body.userId,
-        req.body.classicId,
-        req.body.picturedId,
-        req.body.message
-    ];
+  const sql = 'CALL create_question_report(?, ?, ?, ?)';
+  data = [
+    req.body.userId,
+    req.body.classicId,
+    req.body.picturedId,
+    req.body.message,
+  ];
 
-    pool.getConnection(function (err, conn) {
-        if (err) return res.status(500).json({ message: err.message });
-        conn.query(sql, data, (error, rows) => {
-            conn.release();
-            if (error) return res.status(500).json({ message: error.message });
+  pool.getConnection(function (err, conn) {
+    if (err) return res.status(500).json({ message: err.message });
+    conn.query(sql, data, (error, rows) => {
+      conn.release();
+      if (error) return res.status(500).json({ message: error.message });
 
-            if (rows[0].insertId === 0) {
-                return res.status(500).json({
-                    message: 'The report can not be inserted.'
-                });
-            } else {
-                console.log(rows[0]);
-                console.log(rows[0].insertId);
-                return res.send({ id: rows[0].insertId, userId: req.body.userId, classicId: req.body.classicId, picturedId: req.body.picturedId, message: req.body.message });
-            }
+      if (rows[0].insertId === 0) {
+        return res.status(500).json({
+          message: 'The report can not be inserted.',
         });
+      } else {
+        console.log(rows[0]);
+        console.log(rows[0].insertId);
+        return res.send({
+          id: rows[0].insertId,
+          userId: req.body.userId,
+          classicId: req.body.classicId,
+          picturedId: req.body.picturedId,
+          message: req.body.message,
+        });
+      }
     });
+  });
 });
 
-module.exports = router
+module.exports = router;
